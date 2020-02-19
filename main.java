edf26c885c7776c397953be32436f72f4bf1066d
Jamal Marri <marrij@wwu.edu>
2020-02-18 18:58:48 -0800

    Add test method.

--- main.java
+++ main.java
@@ -1,16 +1,33 @@
-import java.sql.*;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.*;
 import java.util.Scanner;
 
 class main {
     public static void main(String[] args) throws Exception {
-        switch (args[0]) {
-            // cases here
-        }
+        show();
     }
 
-    private static void show(String[] args) {
+    private static void show() throws SQLException, FileNotFoundException {
+        File credentialsFile = new File("credentials.txt");
+        Scanner scan = new Scanner(new FileInputStream(credentialsFile));
+        String url = scan.nextLine();
+        Connection connection = DriverManager.getConnection(url);
+        Statement statement = connection.createStatement();
+        String testQuery = "select emp_no, first_name, last_name from employees where emp_no = 499999;";
+        ResultSet resultSet = statement.executeQuery(testQuery);
 
+        System.out.println("The records selected are:");
+        int rowCount = 0;
+        while (resultSet.next()) {
+            int empNo = resultSet.getInt("emp_no");
+            String firstName = resultSet.getString("first_name");
+            String lastName = resultSet.getString("last_name");
+            System.out.println(empNo + ", " + firstName + ", " + lastName);
+            ++rowCount;
+        }
+        System.out.println("Total number of records = " + rowCount);
     }
 
     private static void add(String[] args) {
